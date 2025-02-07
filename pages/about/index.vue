f1c42b8d7809ca39f0be42d1b4eae971f66a0a6a
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-02-07 19:21:26 +0300

    Enhance About page with interactive button click counter
    - Add reactive number tracking with ref
    - Modify handleClick to increment number with optional parameter
    - Update button click handlers to pass different increment values
    - Display current number on the page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,20 +2,20 @@
 import ButtonGroup from "@/components/ButtonGroup.vue";
 import MyButton from "@/components/MyButton.vue";
 
-const handleClick = () => {
-  console.log("clicked");
+const numb = ref(0);
+const handleClick = (addNum?: number) => {
+  numb.value += addNum ?? 1;
 };
-
-
 </script>
 
 <template>
   <div>
     <h1>About pagessss</h1>
     <ButtonGroup>
-      <MyButton @click="handleClick">sasd</MyButton>
-      <MyButton @click="handleClick">sasd</MyButton>
-      <MyButton @click="handleClick">sasd</MyButton>
+      <MyButton @click="() => handleClick()">sasd</MyButton>
+      <MyButton @click="() => handleClick(2)">sasd</MyButton>
+      <MyButton @click="() => handleClick(3)">sasd</MyButton>
     </ButtonGroup>
+    <span>{{ numb }}</span>
   </div>
 </template>
